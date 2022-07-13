d530b2e66df38bc3672ea83567a75659413120d8
Chris Kratz <Ckratz17@gmail.com>
2022-07-13 15:34:06 -0400

    packages added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
 
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
 const questions = [];
 
